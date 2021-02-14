d698fa0a918011302fae57e703fe56b498621b91
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-02-14 15:01:03 +0900

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="getText">
+    <button @click="showAdress()" type="submit">変換 </button>
+     <p>住所：{{pick}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  
+  data(){
+    return{
+      show:"", 
+      getText:""
+    };
+  },
+   methods:{
+     async showAdress(){
+      let item = await axios.get(`https://apis.postcode-jp.com/api/v3/postcodes/${this.getText}?&apikey=fabCn3nwAAosqqjhmKDAC5fhlZKgNh6fPvWwBZY`);
+      /*const picks = item.data; 
+      this.show = picks.allAddress; */
+      this.show =item.data.allAddress;
+    }
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template >
+  <div id="header">
+    
+  
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      show:"",
+      
+    };
+  },
+  props:["parentText"],
+  method:{
+     async showAdress(){
+      let item = await axios.get('https://apis.postcode-jp.com/api/v4/postcodes/${parenttText}?allAdress');
+     this.show = item.data;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button{
+  height:300px;
+}
+</style>
